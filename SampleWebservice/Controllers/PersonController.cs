ea81a29db7d275ec9ef96868a670bc41c1f458f8
Tobias Verleger <tobias.verleger@seca.com>
2017-07-24 18:42:43 +0200

    Initial Commit

---
+++ SampleWebservice/Controllers/PersonController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SimpleRESTServer.Models;
+
+
+
+namespace SimpleRESTServer.Controllers
+{
+    public class PersonController : ApiController
+    {
+        // GET: api/Person
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Person1", "Person2" };
+        }
+
+        // GET: api/Person/5
+        public Person Get(long id)
+        {
+
+            PersonPersistance pp = new PersonPersistance();
+
+            Person person = pp.getPerson(id);
+
+
+            return person;
+        }
+
+        // POST: api/Person
+        public void Post([FromBody]Person value)
+        {
+        }
+
+        // PUT: api/Person/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Person/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SampleWebservice/PersonPersisitance.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SimpleRESTServer.Models;
+using MySql.Data;
+
+
+namespace SimpleRESTServer
+{
+    public class PersonPersistance
+    {
+
+        private MySql.Data.MySqlClient.MySqlConnection conn;
+
+        public PersonPersistance()
+        {
+
+            string myConnectionString;
+            myConnectionString = "server=127.0.0.1:3306;database=openemr";
+            try
+            {
+
+                conn = new MySql.Data.MySqlClient.MySqlConnection();
+                conn.ConnectionString = myConnectionString;
+                conn.Open();
+
+
+
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+
+
+            }
+        }
+
+        public Person getPerson(long ID)
+        {
+            Person p = new Person();
+            MySql.Data.MySqlClient.MySqlDataReader mySQLReader = null;
+
+            String sqlString = "SELECT pubpid AS pid, lname, fname, DOB from openemr.patient_data";
+            MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand(sqlString, conn);
+
+            mySQLReader = cmd.ExecuteReader();
+            if (mySQLReader.Read())
+            {
+                p.ID = mySQLReader.GetInt32(0);
+                p.FirstName = mySQLReader.GetString(1);
+                p.LastName = mySQLReader.GetString(2);
+                p.StartDate = mySQLReader.GetDateTime(3);
+
+                return p;
+                
+
+
+            }
+            else
+            {
+                return null;
+            }
+
+
+
+
+
+
+        }
+
+        public long savePerson(PersonPersistance pers)
+        {
+
+            String sqlString = "";
+            MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand(sqlString, conn);
+            cmd.ExecuteNonQuery();
+            long id = cmd.LastInsertedId;
+            return id;
+
+
+        }
+
+
+    }
+
+}
